c00ed09cb575f9d90f2256e4dd43345d6c873912
Rakesh Huliyar Ravishankar <30697765+Rakesh-HR@users.noreply.github.com>
2023-07-22 09:10:02 -0700

    Basic initial push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import EsriMapView from './EsriMapView';
+import EsriMapView from './Components/EsriMapView';
+import SearchPage from './Components/SearchPage';
 
 function App() {
   return (
     <div>
       <h1>TrailMatch</h1>
+      <SearchPage></SearchPage>
       <EsriMapView />
     </div>
   );

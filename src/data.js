feef7aa520c06d4df2eab9c4a5a731c4d7df37bc
Rakesh Huliyar Ravishankar <30697765+Rakesh-HR@users.noreply.github.com>
2023-07-23 11:02:31 -0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export default [
       "ImageTitle": "Bad Water Basin",
       "ImageDescription": "Easy out and back trail with beautiful views of the Death Valley salt flats.",
       "ArcGISOnline1": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=4f8029341e6844fda874f20ef82d1c11",
-      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=1eb07c2d23e64a4ca22f639e7baa1b33",
+      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=f17780f5b7c042a0ba62a487de7126c2",
       "ExpBuilder":"https://experience.arcgis.com/experience/48c4207a57ec41ce86a2812314e579c2?data_id=dataSource_1-a87f99a2f0d14f33b55ab5c65b240240%3A3"
     },
     {
@@ -16,7 +16,7 @@ export default [
       "ImageTitle": "Darwin Falls",
       "ImageDescription": "Out and back trail with beautiful water features and wildflower views.",
       "ArcGISOnline1": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=12866d2b427146309ae8105d7b4d3217",
-      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=1eb07c2d23e64a4ca22f639e7baa1b33",
+      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=f17780f5b7c042a0ba62a487de7126c2",
       "ExpBuilder":"https://experience.arcgis.com/experience/48c4207a57ec41ce86a2812314e579c2?data_id=dataSource_1-a87f99a2f0d14f33b55ab5c65b240240%3A3"
     },
     {
@@ -26,7 +26,7 @@ export default [
       "ImageTitle": "Desolation Canyon",
       "ImageDescription": "Moderate trail with stunning views of Death Valley rock formations.",
       "ArcGISOnline1": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=462a699490bf49f3b81e22cf2606b407",
-      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=1eb07c2d23e64a4ca22f639e7baa1b33",
+      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=f17780f5b7c042a0ba62a487de7126c2",
       "ExpBuilder":"https://experience.arcgis.com/experience/48c4207a57ec41ce86a2812314e579c2?data_id=dataSource_1-a87f99a2f0d14f33b55ab5c65b240240%3A3"
     },
     {
@@ -36,7 +36,7 @@ export default [
       "ImageTitle": "Devils Golf Course",
       "ImageDescription": "Easy, flat trail with great views of the mountains on the horizon of Death Valley.",
       "ArcGISOnline1": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=0550e6c7588842cdb914ed123c6abaf1",
-      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=1eb07c2d23e64a4ca22f639e7baa1b33",
+      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=f17780f5b7c042a0ba62a487de7126c2",
       "ExpBuilder":"https://experience.arcgis.com/experience/48c4207a57ec41ce86a2812314e579c2?data_id=dataSource_1-a87f99a2f0d14f33b55ab5c65b240240%3A3"
     },
     {
@@ -46,7 +46,7 @@ export default [
       "ImageTitle": "Golden Canyon and Gower Gulch",
       "ImageDescription": "Moderate trail with slight elevation gain in exchange for a beautiful view at any time of day.",
       "ArcGISOnline1": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=70b1d2ed2325419c8f62f038a5b8ba9a",
-      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=1eb07c2d23e64a4ca22f639e7baa1b33",
+      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=f17780f5b7c042a0ba62a487de7126c2",
       "ExpBuilder":"https://experience.arcgis.com/experience/48c4207a57ec41ce86a2812314e579c2?data_id=dataSource_1-a87f99a2f0d14f33b55ab5c65b240240%3A3"
     },
     {
@@ -56,7 +56,7 @@ export default [
       "ImageTitle": "Golden Canyon to Red Cathedral",
       "ImageDescription": "Moderate trail with end point at Red Cathedral and great views of Death Valley wildlife and nature.",
       "ArcGISOnline1": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=3dd8393d1cb647919dbe091efdfee48a",
-      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=1eb07c2d23e64a4ca22f639e7baa1b33",
+      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=f17780f5b7c042a0ba62a487de7126c2",
       "ExpBuilder":"https://experience.arcgis.com/experience/48c4207a57ec41ce86a2812314e579c2?data_id=dataSource_1-a87f99a2f0d14f33b55ab5c65b240240%3A3"
     },
     {
@@ -66,7 +66,7 @@ export default [
       "ImageTitle": "Hanaupah Canyon",
       "ImageDescription": "Long, beautiful hike with a lot of elevation gain in order to see extensive views of Death Valley National Park.",
       "ArcGISOnline1": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=de2177ec50544994b4d8e8a1d6db3040",
-      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=1eb07c2d23e64a4ca22f639e7baa1b33",
+      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=f17780f5b7c042a0ba62a487de7126c2",
       "ExpBuilder":"https://experience.arcgis.com/experience/48c4207a57ec41ce86a2812314e579c2?data_id=dataSource_1-a87f99a2f0d14f33b55ab5c65b240240%3A3"
     },
     {
@@ -76,7 +76,7 @@ export default [
       "ImageTitle": "Mount Perry",
       "ImageDescription": "A moderate 8-mile hike through Mount Perry with beautiful views of wildlife, wildflowers, and more.",
       "ArcGISOnline1": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=a101295751d34c34ac7720b0dc5340f3",
-      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=1eb07c2d23e64a4ca22f639e7baa1b33",
+      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=f17780f5b7c042a0ba62a487de7126c2",
       "ExpBuilder":"https://experience.arcgis.com/experience/48c4207a57ec41ce86a2812314e579c2?data_id=dataSource_1-a87f99a2f0d14f33b55ab5c65b240240%3A3"
     },
     {
@@ -86,7 +86,7 @@ export default [
       "ImageTitle": "Telescope Peak",
       "ImageDescription": "Difficult level trail with moderate elevation gain. Out and back in 12 miles with rewarding views of Telescope Peak.",
       "ArcGISOnline1": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=ef17dc2691f145eca0e93bf3827cc9f1",
-      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=1eb07c2d23e64a4ca22f639e7baa1b33",
+      "ArcGISOnline2": "https://intern-hackathon.maps.arcgis.com/apps/instant/basic/index.html?appid=f17780f5b7c042a0ba62a487de7126c2",
       "ExpBuilder":"https://experience.arcgis.com/experience/48c4207a57ec41ce86a2812314e579c2?data_id=dataSource_1-a87f99a2f0d14f33b55ab5c65b240240%3A3"
     }
   ]
